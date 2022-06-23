6bac00d380a8894e205260e875e003114b109a1f
auslin-aot <auslin.pajila@aot-technologies.com>
2022-06-23 18:45:03 +0530

    test case

--- forms-flow-api/tests/conftest.py
+++ forms-flow-api/tests/conftest.py
@@ -139,7 +139,7 @@ def auto(docker_services, app):
         docker_services.start('bpm')
         docker_services.start('analytics')
         docker_services.start('formio')
-        docker_services.start('proxy')       
+        docker_services.start('proxy')
 
 
 @pytest.fixture(scope="session")
