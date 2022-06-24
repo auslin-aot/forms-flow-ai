4539ebfdbfad7e2a107dc225daea7893b0c6a59b
auslin-aot <auslin.pajila@aot-technologies.com>
2022-06-24 10:53:44 +0530

    test case updates

--- forms-flow-api/tests/conftest.py
+++ forms-flow-api/tests/conftest.py
@@ -138,7 +138,7 @@ def auto(docker_services, app):
 
         docker_services.start('bpm')
         docker_services.start('analytics')
-        docker_services.start('formio')
+        docker_services.start('forms')
         docker_services.start('proxy')
 
 
